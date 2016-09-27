af0cb938dff6cacda9afab1284b798441f1d5526
Francisco Ivanio <franciscoivanio@gmail.com>
2016-09-27 11:36:00 -0300

    instalar pacotes

--- scripts/seed.py
+++
@@ -1,7 +0,0 @@
-import psycopg2
-
-try:
-    conn = psycopg2.connect("dbname='hackton' user='hackton' host='10.0.1.19' password='CHUBACA1234'")
-    print "connection OK"
-except:
-    print "I am unable to connect to the database"
